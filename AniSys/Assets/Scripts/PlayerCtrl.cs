57e03d040e6903fcee283715ac1819e757ec2267
UnitySir <zoujack@foxmail.com>
2020-08-26 20:50:18 +0800

    Unity动画系统学习

---
+++ AniSys/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+    private Animator playerAni;
+
+    public bool isWalk;
+    public bool isRun;
+    public bool isJump;
+
+    private float targetRun;
+
+    private void Awake() {
+        playerAni = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    private void Update() {
+        isWalk = Input.GetKey(KeyCode.W);
+        isRun = Input.GetKey(KeyCode.LeftShift);
+        isJump = Input.GetKey(KeyCode.Space);
+        float targetRun = isWalk ? (isRun ? 2f : 1f) : 0f;
+        playerAni.SetFloat("move", Mathf.Lerp(playerAni.GetFloat("move"),targetRun,0.5f));
+        if (isJump) {
+            playerAni.SetTrigger("jump");
+        }
+    }
+}
